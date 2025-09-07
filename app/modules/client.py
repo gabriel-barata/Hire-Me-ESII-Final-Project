b3638d4b4c5667f029b06d4eb8311a48e382b0fb
F-Hecame <fabio.jayme@hotmail.com>
2025-09-07 15:29:35 -0300

    alteração de operadores

--- app/modules/client.py
+++ app/modules/client.py
@@ -87,7 +87,7 @@ def delet(table):
 
 def sort_alljobs(table):
     criteria = search_d.get()
-    if not criteria != "Select":
+    if not criteria == "Select":
         table.delete(*table.get_children())
 
         with db_connection.managed_cursor() as cur:
@@ -113,7 +113,7 @@ def sort_alljobs(table):
 
 def sort_myapplications(table):
     criteria = search_d.get()
-    if not criteria != "Select":
+    if not criteria == "Select":
         table.delete(*table.get_children())
 
         with db_connection.managed_cursor() as cur:
