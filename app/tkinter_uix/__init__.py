4e157e9255260ab1893f22fc1f50a49e4f90cc47
F-Hecame <fabio.jayme@hotmail.com>
2025-09-07 16:31:58 -0300

    Rename field 'theme'

--- app/tkinter_uix/__init__.py
+++ app/tkinter_uix/__init__.py
@@ -10,7 +10,8 @@ class Theme:
             str(THEMES_FOLDER / f"{name}.yaml"), "r", encoding="UTF-8"
         ) as file:
             self.data = yaml.load(file, Loader=yaml.FullLoader)
-        self.app_color = self.theme["App"]
-        self.btn_color = self.theme["Button"]
-        self.entry_color = self.theme["Entry"]
-        self.navbar_color = self.theme["Navbar"]
+            
+        self.app_color = self.data["App"]
+        self.btn_color = self.data["Button"]
+        self.entry_color = self.data["Entry"]
+        self.navbar_color = self.data["Navbar"]
